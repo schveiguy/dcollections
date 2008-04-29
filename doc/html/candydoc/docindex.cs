0ec6e0724a3a249cd6040a58ef7b6469dfdb84c1
schveiguy <schveiguy@079a8277-aa6b-4962-891b-5f7b574ea669>
2008-04-29 14:35:48 +0000

    updated to have dcollections stuff instead of tango stuff

--- doc/html/candydoc/docindex.cs
+++ doc/html/candydoc/docindex.cs
@@ -3,102 +3,98 @@
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<meta name="description" content="Platform Library for the D Programming Language">
-<meta name="keywords" content="tango, d, d programming language, dmd, gdc, pirates, avast, matey">
-
- <title>About Trac - tango - Trac</title><link rel="start" href="/projects/tango/wiki" /><link rel="search" href="/projects/tango/search" /><link rel="help" href="/projects/tango/wiki/TracGuide" /><link rel="stylesheet" href="/projects/tango/chrome/common/css/trac.css" type="text/css" /><link rel="stylesheet" href="/projects/tango/chrome/common/css/dsource.css" type="text/css" /><link rel="stylesheet" href="/projects/tango/chrome/common/css/about.css" type="text/css" /><link rel="stylesheet" href="/projects/tango/themeengine/theme.css" type="text/css" /><link rel="icon" href="/projects/tango/chrome/common/dsource.ico" type="image/x-icon" /><link rel="shortcut icon" href="/projects/tango/chrome/common/dsource.ico" type="image/x-icon" /><style type="text/css">
+<title>dcollections - dsource.org</title>
+<meta name="description" content="Open Source Development for the D Programming Language">
+<meta name="keywords" content="dsource, d programming, d programming language, subversion, trac">
+<link rel="start" href="/projects/dcollections/wiki" /><link rel="search" href="/projects/dcollections/search" /><link rel="help" href="/projects/dcollections/wiki/TracGuide" /><link rel="stylesheet" href="/projects/dcollections/chrome/common/css/trac.css" type="text/css" /><link rel="stylesheet" href="/projects/dcollections/chrome/common/css/dsource.css" type="text/css" /><link rel="stylesheet" href="/projects/dcollections/chrome/common/css/wiki.css" type="text/css" /><link rel="stylesheet" href="/projects/dcollections/pygments/trac.css" /><link rel="icon" href="/projects/dcollections/chrome/common/dsource.ico" type="image/x-icon" /><link rel="shortcut icon" href="/projects/dcollections/chrome/common/dsource.ico" type="image/x-icon" /><link rel="alternate" href="/projects/dcollections/wiki/WikiStart?format=txt" title="Plain Text" type="text/x-trac-wiki" /><link rel="alternate" href="/projects/dcollections/wiki/WikiStart?format=txt" title="Plain Text" type="text/x-trac-wiki" /><link rel="alternate" href="/projects/dcollections/wiki/WikiStart?format=txt" title="Plain Text" type="text/x-trac-wiki" />
+<style type="text/css">
 </style>
- <script type="text/javascript" src="/projects/tango/chrome/common/js/trac.js"></script>
- <script type="text/javascript" src="/projects/tango/chrome/common/js/dsource.js"></script>
+<script type="text/javascript" src="/projects/dcollections/chrome/common/js/trac.js"></script>
 
- <script type="text/javascript" src="/projects/tango/chrome/../themeengine/theme.js"></script>
+<script type="text/javascript" src="/projects/dcollections/chrome/common/js/dsource.js"></script>
 </head>
-<body>
- <div class="theme-outside" align="center">
-  <div class="theme-page" id="page" align="left">
-   <table cellpadding="0" cellspacing="0" border="0" width="100%">
-    <tr>
-     <td valign="top" background="/projects/tango/chrome/theme/images/Page-Left.png" width="16">
-      <img src="/projects/tango/chrome/theme/images/Page-Top-Left.png" /></td>
+<body onload="initNav();">
 
-     <td valign="bottom">
-      <div class="theme-head-outer" align="left">
-       <div class="theme-head-inner" align="right">
-        <table cellpadding="0" cellspacing="0" border="0" width="100%">
-         <tr>
-          <td align="left">
-           <a href="/projects/tango">
-            <img src="/projects/tango/chrome/theme/images/Tango-Logo.png" 
-                 border="0"/>
-           </a>
+<div id="container">
+ 
+<div id="siteheader">
+<div id="left">
+<div id="logo"><img src="/chrome/dsource/img/dsource-logo.gif" /></div>
+<div id="title">dsource.org</div>
 
-          </td>
-          <td align="right">
-           <table cellpadding="0" cellspacing="0" border="0">
-            <tr>
-             <td><a href="/projects/tango/wiki/Download"><img src="/projects/tango/chrome/theme/images/Download.png" border="0" 
-              alt="Download" title="Download"/></a></td>
-             <td><a href="/projects/tango/wiki/Manual"><img src="/projects/tango/chrome/theme/images/Reference.png" border="0" 
-              alt="Reference Manual" title="Reference Manual"/></a></td>
-            </tr>
-           </table>
-          </td>
+<div id="tag">Open Source Development for<br />
+the D Programming Language</div>
+</div>
 
-         </tr>
-         <tr>
-          <td align="left" valign="top" class="theme-head-menu-item-normal">
-           Platform Library for the D Programming Language
-          </td>
-          <td align="right">
-           <table cellpadding="0" cellspacing="0" border="0">
-            <tr>
+<div id="right">
+<ul>
+<li><a href="/">Home</a></li>
+<li><a id="current" href="/projects/">Projects</a></li>
+
+<li><a href="/forums/"><span>Forums</span></a></li>
+<li><a href="/projects/tutorials/wiki"><span>Tutorials</span></a></li>
+<li><a href="/site/"><span>Site</span></a></li>
+</ul>
+</div>
+</div>
+                                                                
+<div id="mainpage">
 
 
+<div id="banner">
 
-             <td class="theme-head-menu-item-normal" valign="top">
-              <a href="/projects/tango/wiki/About">About</a>
-             </td>
 
-             <td class="theme-head-menu-item-normal" valign="top">
-              <a href="/projects/tango/wiki">Wiki</a>
-             </td>
+<div id="header"><h1><a href="http://www.dsource.org/">dcollections</a></h1></div>
 
-             <td class="theme-head-menu-item-normal" valign="top">
-              <a href="/projects/tango/forums">Forums</a>
-             </td>
+</div>
 
-             <td class="theme-head-menu-item-normal" valign="top">
-              <a href="/projects/tango/browser/">Source</a>
-             </td>
 
-             <td class="theme-head-menu-item-normal" valign="top">
-              <a href="/projects/tango/search">Search</a>
-             </td>
+  <div id="nav_all">
+   <div id="mainnav" class="nav"><ul><li class="active first"><a accesskey="1" href="/projects/dcollections/wiki">Wiki</a></li><li><a accesskey="2" href="/projects/dcollections/timeline">Timeline</a></li><li><a accesskey="3" href="/projects/dcollections/roadmap">Roadmap</a></li><li><a href="/projects/dcollections/browser">Browse Source</a></li><li><a href="/projects/dcollections/report">View Tickets</a></li><li><a accesskey="7" href="/projects/dcollections/newticket">New Ticket</a></li><li><a accesskey="4" href="/projects/dcollections/search">Search</a></li><li class="last"><a href="/projects/dcollections/admin">Admin</a></li></ul></div>
 
-             <td class="theme-head-menu-item-normal" valign="top">
-              <a href="/projects/tango/wiki/Contact">Contact</a>
-             </td>
+   <div id="metanav" class="nav"><ul><li class="first">logged in as schveiguy</li><li><a href="/forums/login.php?logout=true&amp;sid=&amp;redirect=../projects/dcollections/wiki">Logout</a></li><li><a accesskey="6" href="/projects/dcollections/wiki/TracGuide">Help/Guide</a></li><li class="last"><a href="/projects/dcollections/about">About Trac</a></li></ul></div>
+   <div id="searchnav">
+   <form id="search" action="/projects/dcollections/search" method="get">
+    <div>
+     <label for="proj-search">Search:</label>
+     <input type="text" id="proj-search" name="q" size="10" accesskey="f" value="" />
 
-            </tr>
+     <input type="submit" value="Search" />
+     <input type="hidden" name="wiki" value="on" />
+     <input type="hidden" name="changeset" value="on" />
+     <input type="hidden" name="ticket" value="on" />
+    </div>
+   </form>
+   </div>
+  </div>
 
-           </table>
-          </td>
-         </tr>
-        </table>
-       </div>
-      </div>
-      
-      <div class="theme-content-outer" align="left">
-       <div class="theme-content-inner" align="left">
-        <div class="theme-content-block">
+  <div id="main">
 
-         <div id="ctxtnav" class="nav"></div>
-         <div id="content" class="wiki">
-          <div class="wikipage">
-           <div id="searchable"> 
 
-  <h2>Tango API Index</h2>
+
+
+<div id="ctxtnav" class="nav">
+ <h2>Wiki Navigation</h2>
+ <ul>
+   <li><a href="/projects/dcollections/wiki">Start Page</a></li>
+   <li><a href="/projects/dcollections/wiki/TitleIndex">Index by Title</a></li>
+
+   <li><a href="/projects/dcollections/wiki/RecentChanges">Index by Date</a></li>
+   <li class="last"><a href="/projects/dcollections/wiki/WikiStart?action=diff&amp;version=10">Last Change</a></li>
+ </ul>
+ <hr />
+</div>
+
+<div id="content" class="wiki">
+
+ 
+  
+  
+   
+   <div class="wikipage">
+    <div id="searchable">
+
+  <h2>DCollections API Index</h2>
   <p>Use the Browser incremental search facility to quickly locate a particular
     module name.</p>
   <p>Source code for each module is available by clicking the <a href="#">big
@@ -113,113 +109,30 @@
            </div>
           </div>
          </div>
-        </div> <!-- End of Content Area -->
-       </div>
-      </div>
-     </td>
-     <td valign="top" background="/projects/tango/chrome/theme/images/Page-Right.png" width="16"><img src="/projects/tango/chrome/theme/images/Page-Top-Right.png" /></td>
-    </tr>
+     <!--CONTENTEND-->
+    </div>
+   </div>
+</div>
 
-    <tr>
-     <td width="16" background="/projects/tango/chrome/theme/images/Page-Left.png" valign="bottom"><img src="/projects/tango/chrome/theme/images/Page-Bottom-Left.png" /></td>
-     <td valign="bottom">
-      <div class="theme-foot-outer" align="left">
-       <div class="theme-foot-inner" align="left">
-        <table cellpadding="0" cellspacing="0" border="0" width="100%">
-         <tr>
-          <td valign="bottom" style="height: 37px;">
-           <table cellpadding="0" cellspacing="0" border="0">
+<script type="text/javascript">searchHighlight()</script>
 
-            <tr>
+<div id="altlinks"><h3>Download in other formats:</h3><ul><li class="first"><a href="/projects/dcollections/wiki/WikiStart?format=txt">Plain Text</a></li><li><a href="/projects/dcollections/wiki/WikiStart?format=txt">Plain Text</a></li><li class="last"><a href="/projects/dcollections/wiki/WikiStart?format=txt">Plain Text</a></li></ul></div>
 
-
-
-
-             <td class="theme-foot-menu-item-normal" valign="bottom">
-              <a href="/projects/tango/roadmap">Roadmap</a>
-             </td>
-
-             <td class="theme-foot-menu-item-normal" valign="bottom">
-              <a href="/projects/tango/timeline">Timeline</a>
-
-             </td>
-
-             <td class="theme-foot-menu-item-normal" valign="bottom">
-              <a href="/projects/tango/report">Tickets</a>
-             </td>
-
-             <td class="theme-foot-menu-item-normal" valign="bottom">
-              <a href="/projects/tango/newticket">New Ticket</a>
-             </td>
-
-             <td class="theme-foot-menu-item-normal" valign="bottom">
-              <a href="/projects/tango/wiki/TracGuide">Help / Guide</a>
-             </td>
-
-             <td class="theme-foot-menu-item-normal" valign="bottom">
-              <a href="/projects/tango/about">About Trac</a>
-             </td>
-
-            </tr>
-           </table>
-          </td>
-          <td align="right" valign="bottom" class="theme-foot-menu-item-normal">
-           &nbsp; 
-          </td>
-         </tr>
-
-         <tr>
-          <td align="left">
-           <table cellpadding="0" cellspacing="0" border="0">
-            <tr>
-             <td><a href="http://trac.edgewall.org"><img src="/projects/tango/chrome/theme/images/Trac.png" border="0" alt="Trac" title="powered by Trac"/></a></td>
-             <td><a href="http://www.dsource.org"><img src="/projects/tango/chrome/theme/images/DSource.png" border="0" alt="dsource.org" title="powered by dsource.org"/></a></td>
-            </tr>
-           </table>
-          </td>
-
-          <td align="right">
-           <table cellpadding="0" cellspacing="0" border="0">
-            <tr>
-             <td>&nbsp;</td>
-            </tr>
-           </table>
-          </td>
-         </tr>
-        </table>
-
-       </div>
-      </div>
-      <table cellpadding="0" cellspacing="0" border="0" width="100%">
-       <tr>
-        <td align="left" background="/projects/tango/chrome/theme/images/Page-Bottom.png"><img src="/projects/tango/chrome/theme/images/Page-Inner-Bottom-Left.png" /></td>
-        <td align="right" background="/projects/tango/chrome/theme/images/Page-Bottom.png"><img src="/projects/tango/chrome/theme/images/Page-Inner-Bottom-Right.png" /></td>
-       </tr>
-      </table>
-     </td>
-
-     <td width="16" background="/projects/tango/chrome/theme/images/Page-Right.png" valign="bottom"><img src="/projects/tango/chrome/theme/images/Page-Bottom-Right.png" /></td>
-    </tr>
-   </table>
-   
-   <div class="theme-copyright" align="center">Copyright &copy; 2006-2007 Tango. All Rights Reserved. | Page Width: <a href="javascript:goStatic();">Static</a> or <a href="javascript:goDynamic();">Dynamic</a></div>
-   
   </div>
-
  </div>
+ 
+ <div id="sitefooter">
+  <p class="left">&copy; Copyright 2004-2008 - The Sankaty Group, Inc.
+   All rights reserved.</p>
+  <p class="right">
 
- <script type="text/javascript">
-  /* highlight search terms */
-  searchHighlight();
-  
-  /* Rememeber the page width setting */
-  if(readCookie("pagewidth") == 'dynamic') {
-    goDynamic();
-  } else {
-    goStatic();
-  }
- </script>
+   <a href="/site/donate">Make a Donation</a><br /><br />
+   <a id="priv" href="/site/privacy">Privacy Statement</a>&nbsp;&nbsp;
+   <a id="tos" href="/site/terms">Terms of Service</a>
+  </p>
+ </div>
+</div>
 
  </body>
-</html>
 
+</html>
