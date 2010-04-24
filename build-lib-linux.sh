151e0e4d6672852e17e9a197b2f748479799936d
schveiguy <schveiguy@079a8277-aa6b-4962-891b-5f7b574ea669>
2010-04-24 03:22:22 +0000

    Added ability to do unit tests

--- build-lib-linux.sh
+++ build-lib-linux.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+if [ "$1" = 'unittest' ]
+then
+    unittest='-unittest'
+fi
+
 rm -f *.o
-dmd -c dcollections/*.d
+dmd -c $unittest dcollections/*.d
 rm -f libdcollections.a
 ar ruv libdcollections.a *.o
